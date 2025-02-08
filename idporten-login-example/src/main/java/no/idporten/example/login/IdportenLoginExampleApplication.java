a0084cced76f416b402b796fcb2690af39d5da64
Anders Holst <anders.lietzen.holst@digdir.no>
2025-02-08 13:02:48 +0100

    Add template idea project

---
+++ idporten-login-example/src/main/java/no/idporten/example/login/IdportenLoginExampleApplication.java
@@ -0,0 +1,12 @@
+package no.idporten.example.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdportenLoginExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdportenLoginExampleApplication.class, args);
+    }
+}
