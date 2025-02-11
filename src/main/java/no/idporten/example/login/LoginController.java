19dabaf6c1421a6ae93430815ddfc577b1223e86
Anders Holst <anders.lietzen.holst@digdir.no>
2025-02-11 13:01:08 +0100

    Cleanup

--- src/main/java/no/idporten/example/login/LoginController.java
+++ src/main/java/no/idporten/example/login/LoginController.java
@@ -10,36 +10,33 @@ import com.nimbusds.oauth2.sdk.pkce.CodeVerifier;
 import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import com.nimbusds.openid.connect.sdk.Nonce;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.net.URI;
 
 @Controller
 public class LoginController {
-
-    // TODO
-    // @Value("@{.../callbackURI}")
-    // private final String callbackURIStr;
+    private final String clientIDStr = "oidc_idporten_example_login";
+    private final String callbackURIStr = "http://localhost:7040/callback";
+    private final String endpointURIStr = "https://login.idporten.dev/authorize";
 
     // used to check state in authorization responses.
     private State lastState;
 
     @GetMapping(path = "/login")
-    public String login(Model model) { // TODO: need model?
+    public String login() {
 
-        ClientID clientID = new ClientID("oidc_idporten_example_login");
+        ClientID clientID = new ClientID(clientIDStr);
 
         // can use URI.create() since we know the addresses to be valid.
-        // TODO: what happens if callback/endpoint address/port change?
-        URI callbackURI = URI.create("http://localhost:7040/callback");
-        URI endpointURI = URI.create("https://login.idporten.dev/authorize");
+        URI callbackURI = URI.create(callbackURIStr);
+        URI endpointURI = URI.create(endpointURIStr);
 
         State state = new State();
 
         AuthenticationRequest request =
             new AuthenticationRequest.Builder(
-                ResponseType.CODE, // authorization code flow
+                ResponseType.CODE,   // use authorization code flow.
                 new Scope("openid"), // TODO: +profile?
                 clientID,
                 callbackURI
@@ -49,16 +46,10 @@ public class LoginController {
              .codeChallenge(new CodeVerifier(), CodeChallengeMethod.S256) // PKCE
              .build();
 
+        // store state for verification in callback.
         lastState = state;
 
         String requestURIStr = request.toURI().toString();
         return "redirect:" + requestURIStr;
-    }
-
-
-    @GetMapping(path = "/callback")
-    public String loginCallback() {
-        // TODO
-        return "";
     }
 }
