c4f38d5c829afe72659c1c5edaa218f08d5a6c7e
Anders L Holst <36549217+sortraev@users.noreply.github.com>
2025-02-14 12:52:51 +0100

    ID-5092: receive authorization responses (#4)

--- src/main/java/no/idporten/example/login/LoginController.java
+++ src/main/java/no/idporten/example/login/LoginController.java
@@ -1,25 +1,34 @@
 package no.idporten.example.login;
 
+import com.nimbusds.oauth2.sdk.*;
+import com.nimbusds.oauth2.sdk.id.State;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import com.nimbusds.oauth2.sdk.ResponseType;
 import com.nimbusds.oauth2.sdk.Scope;
 import com.nimbusds.oauth2.sdk.id.ClientID;
-import com.nimbusds.oauth2.sdk.id.State;
 import com.nimbusds.oauth2.sdk.pkce.CodeChallengeMethod;
 import com.nimbusds.oauth2.sdk.pkce.CodeVerifier;
 
 import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import com.nimbusds.openid.connect.sdk.Nonce;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.net.URI;
 
 @Controller
 public class LoginController {
+
+
     private final String clientIDStr = "oidc_idporten_example_login";
     private final String callbackURIStr = "http://localhost:7040/callback";
     private final String endpointURIStr = "https://login.idporten.dev/authorize";
+    final URI callbackURI = URI.create("http://localhost:7040/callback");
 
     @GetMapping(path = "/login")
     public String login(HttpSession session) {
@@ -49,5 +58,53 @@ public class LoginController {
 
         String requestURIStr = request.toURI().toString();
         return "redirect:" + requestURIStr;
+    }
+
+    @GetMapping(path = "/callback")
+    public String loginCallback(HttpServletRequest req, HttpSession session, Model model) {
+
+        URI authzResponseURI = UriComponentsBuilder.fromUri(callbackURI)
+                                                   .query(req.getQueryString())
+                                                   .build()
+                                                   .toUri();
+        return handleCallbackURI(authzResponseURI, session, model);
+    }
+
+    private String handleCallbackURI(URI authzResponseURI, HttpSession session, Model model) {
+        AuthorizationResponse resp;
+        try {
+            resp = AuthorizationResponse.parse(authzResponseURI);
+        }
+        catch (ParseException e) {
+            model.addAttribute("errmsg_attr", "Authorization response parse error");
+            return "login_fail";
+        }
+
+        if (!resp.indicatesSuccess()) {
+            model.addAttribute("errmsg_attr", resp.toErrorResponse());
+            return "login_fail";
+        }
+
+        // authz code flow requires that state received in an authz response
+        // should match state sent in authz request.
+        // this should be stored in session from the authorization request.
+        State lastState = (State) session.getAttribute("lastState");
+        if (lastState == null) {
+            model.addAttribute("errmsg_attr", "No stored state found");
+            return "login_fail";
+        }
+        if (!resp.getState().equals(lastState)) {
+            model.addAttribute("errmsg_attr", "Bad state");
+            return "login_fail";
+        }
+
+        model.addAttribute("errmsg_attr", "Success");
+
+        String authzCode = resp.toSuccessResponse()
+                               .getAuthorizationCode()
+                               .getValue();
+        model.addAttribute("authz_code_attr", authzCode);
+
+        return "login_success";
     }
 }
