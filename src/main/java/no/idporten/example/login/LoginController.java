a67d3f123bf940a6e74167bb1deda259bfd870b1
Anders Holst <anders.lietzen.holst@digdir.no>
2025-02-18 12:23:24 +0100

    Validate ID tokens

--- src/main/java/no/idporten/example/login/LoginController.java
+++ src/main/java/no/idporten/example/login/LoginController.java
@@ -1,15 +1,22 @@
 package no.idporten.example.login;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jwt.JWT;
 import com.nimbusds.oauth2.sdk.*;
 import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
 import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
 import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.oauth2.sdk.id.State;
 
 import com.nimbusds.oauth2.sdk.token.Tokens;
+import com.nimbusds.openid.connect.sdk.Nonce;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponse;
-import com.nimbusds.openid.connect.sdk.Prompt;
-import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
+import com.nimbusds.openid.connect.sdk.claims.ClaimsSet;
+import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
@@ -27,10 +34,11 @@ import com.nimbusds.oauth2.sdk.pkce.CodeChallengeMethod;
 import com.nimbusds.oauth2.sdk.pkce.CodeVerifier;
 
 import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
-import com.nimbusds.openid.connect.sdk.Nonce;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.Objects;
 
 @Controller
@@ -48,16 +56,22 @@ public class LoginController {
     private final String authzEndpointURIStr = "https://login.idporten.dev/authorize";
     private final String tokenEndpointURIStr = "https://idporten.dev/token";
 
+    private final String jwksEndpointURIStr = "https://idporten.dev/jwks.json";
+
+    private final String issuerURIStr = "https://idporten.dev";
+
     private final URI callbackURI = URI.create(callbackURIStr);
+
     private final URI authzEndpointURI = URI.create(authzEndpointURIStr);
     private final URI tokenEndpointURI = URI.create(tokenEndpointURIStr);
+
+    private final URI jwksEndpointURI = URI.create(jwksEndpointURIStr);
+    private final URI issuerURI = URI.create(issuerURIStr);
 
     @GetMapping(path = "/login")
     public String login(HttpSession session) {
 
-        State state = new State();
-        Nonce nonce = new Nonce();
-        CodeVerifier codeVerifier = new CodeVerifier();
+        ProtocolVerifier myVerifier = new ProtocolVerifier();
 
         AuthenticationRequest request =
             new AuthenticationRequest.Builder(
@@ -66,25 +80,23 @@ public class LoginController {
                 clientID,
                 callbackURI
             ).endpointURI(authzEndpointURI)
-             .state(state)
-             .nonce(nonce)
-             .codeChallenge(codeVerifier, CodeChallengeMethod.S256) // PKCE
-             .prompt(Prompt.Type.LOGIN)
+             .state(myVerifier.state())
+             .nonce(myVerifier.nonce())
+             .codeChallenge(myVerifier.codeVerifier(), CodeChallengeMethod.S256)
              .build();
 
         // store state for verification in callback.
-        session.setAttribute("state", state);
-        session.setAttribute("nonce", nonce);
-        session.setAttribute("code_verifier", codeVerifier);
+        session.setAttribute("protocol_verifier", myVerifier);
 
         String requestURIStr = request.toURI().toString();
         return "redirect:" + requestURIStr;
     }
 
     @GetMapping(path = "/callback")
-    public String loginCallback(HttpServletRequest req,
-                                HttpSession session,
-                                Model model) {
+    public String loginCallback(
+        HttpServletRequest req,
+        HttpSession session,
+        Model model) {
         URI authzResponseURI =
             UriComponentsBuilder.fromUri(callbackURI)
                                 .query(req.getQueryString())
@@ -94,35 +106,62 @@ public class LoginController {
         AuthorizationCode authzCode =
             getAuthzCodeFromAuthzResponse(authzResponseURI, session);
 
-        model.addAttribute("authz_code_attr", authzCode);
-
         AccessTokenResponse tokenResp = getAccessTokenResp(authzCode, session);
         Tokens tokens = tokenResp.getTokens();
 
-        // TODO: which tokens do we need..?
+        // TODO: which tokens do we need to keep around..?
         session.setAttribute("access_token", tokens.getAccessToken().getValue());
         session.setAttribute("refresh_token", tokens.getRefreshToken().getValue());
 
-        model.addAttribute("access_token", tokens.getAccessToken().getValue());
-        model.addAttribute("refresh_token", tokens.getRefreshToken().getValue());
-
         if (tokenResp instanceof OIDCTokenResponse oidcTokenResp) {
-            // TODO: validate OIDC token! but how?
-            OIDCTokens oidcTokens = oidcTokenResp.getOIDCTokens();
 
-            session.setAttribute("oidc_token", oidcTokens.getIDToken().toString());
-            model.addAttribute("oidc_token", oidcTokens.getIDToken().toString());
+            JWT oidcToken = oidcTokenResp.getOIDCTokens().getIDToken();
+            session.setAttribute("oidc_token", oidcToken.getParsedString());
+
+            ClaimsSet claims = validateIdToken(oidcToken, session);
+
+            String pidStr = (String) claims.getClaim("pid");
+            if (pidStr == null) {
+                throw new MyLoginException("No pid in claims!");
+            }
+            // TODO: d- eller f-nr?
+
+            logger.info("Got pid from claims");
+            model.addAttribute("pid", pidStr);
         }
 
         return "login_success";
     }
 
-    // TODO: name and signature.
-    private AccessTokenResponse getAccessTokenResp(AuthorizationCode authzCode,
-                                                   HttpSession session) {
+    private ClaimsSet validateIdToken(JWT token, HttpSession session) {
+        try {
+            Issuer issuer = new Issuer(issuerURI);
+
+            // TODO: acquire these dynamically..?
+            JWSAlgorithm jwsAlgorithm = new JWSAlgorithm("RS256");
+            URL jwkSetUrl = jwksEndpointURI.toURL();
+
+            IDTokenValidator idTokenValidator =
+                new IDTokenValidator(issuer, clientID, jwsAlgorithm, jwkSetUrl);
+
+            Nonce expectedNonce =
+                ProtocolVerifier.fromHttpSession(session).nonce();
+
+            ClaimsSet claims = idTokenValidator.validate(token, expectedNonce);
+            logger.info("Token validation successful");
+            return claims;
+        }
+        catch (Exception e) {
+            throw new MyLoginException("ID validation failed", e);
+        }
+    }
+
+    private AccessTokenResponse getAccessTokenResp(
+        AuthorizationCode authzCode,
+        HttpSession session) {
         try {
             CodeVerifier codeVerifier =
-                (CodeVerifier) session.getAttribute("code_verifier");
+                ProtocolVerifier.fromHttpSession(session).codeVerifier();
 
             AuthorizationGrant codeGrant =
                 new AuthorizationCodeGrant(authzCode, callbackURI, codeVerifier);
@@ -130,11 +169,9 @@ public class LoginController {
             ClientAuthentication clientAuthn =
                 new ClientSecretBasic(clientID, clientSecret);
 
-            // TODO: what should the scope be here? using TokenRequest without
-            // TODO: scope is deprecated.
-            // Scope scope = new Scope("openid", "profile");
+            Scope scope = new Scope("openid");
             TokenRequest tokenReq =
-                new TokenRequest(tokenEndpointURI, clientAuthn, codeGrant);//, scope);
+                new TokenRequest(tokenEndpointURI, clientAuthn, codeGrant, scope);
 
             TokenResponse tokenResp =
                 OIDCTokenResponse.parse(tokenReq.toHTTPRequest().send());
@@ -166,7 +203,7 @@ public class LoginController {
             // authz code flow requires that state received in an authz response
             // should match state sent in authz request.
             // this should be stored in session from the authorization request.
-            State lastState = (State) session.getAttribute("state");
+            State lastState = ProtocolVerifier.fromHttpSession(session).state();
             if (lastState == null) {
                 throw new MyLoginException("No stored state found");
             }
